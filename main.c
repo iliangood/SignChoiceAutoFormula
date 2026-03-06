a8151e505b209c7822904c4a79d1d764db373ead
iliangood <62498803+iliangood@users.noreply.github.com>
2026-03-06 13:34:46 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
-#include "stdlib.h"
-#include "stdio.h"
-#include "stdbool.h"
-#include "string.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 #include "fractions.h"
 
 int getCharsN(int number) {
